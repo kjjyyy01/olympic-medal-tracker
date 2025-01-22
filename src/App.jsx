f4197208abb61417f67a8b563f663ccdbe148879
Jongyeon Kim <tyjysong0712@gmail.com>
2025-01-22 18:51:02 +0900

    #1 Create 및 음수 입력 불가 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,115 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
 
 const App = () => {
-    const medalColor = ["Gold", "Silver", "Bronze"];
     const tbodyTitle = ["Country Name", "Gold", "Silver", "Bronze", "Delete"];
-    const countries = [
-        {
-            country: "대한민국",
-            gold: 10,
-            silver: 10,
-            bronze: 10,
-        },
-        {
-            country: "프랑스",
-            gold: 3,
-            silver: 5,
-            bronze: 6,
-        },
-        {
-            country: "영국",
-            gold: 1,
-            silver: 2,
-            bronze: 3,
-        },
-    ];
+    const [talk, setTalk] = useState("추가된 정보가 없습니다.");
+    const [countries, setCountries] = useState([]);
+
+    const [countryName, setCountryName] = useState("");
+    const [gold, setGold] = useState("");
+    const [silver, setSilver] = useState("");
+    const [bronze, setBronze] = useState("");
     return (
         <div id="container">
             <header className="header-container">
                 <h1 className="title">2024 Paris Olympic</h1>
                 <div>
                     <label htmlFor="">Country Name</label>
-                    <input type="text" />
+                    <input
+                        value={countryName}
+                        onChange={(e) => {
+                            setCountryName(e.target.value);
+                        }}
+                        type="text"
+                    />
                 </div>
-                {medalColor.map((color) => {
-                    return (
-                        <div key={color}>
-                            <label htmlFor="">{color}</label>
-                            <input type="number" />
-                        </div>
-                    );
-                })}
-                <button>Add</button>
+                <div>
+                    <label htmlFor="">Gold</label>
+                    <input
+                        value={gold}
+                        onChange={(e) => {
+                            setGold(e.target.value);
+                            if (e.target.value < 0) {
+                                alert("음수는 사용할 수 없습니다.");
+                                setGold("");
+                                return;
+                            }
+                        }}
+                        type="number"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="">Silver</label>
+                    <input
+                        value={silver}
+                        onChange={(e) => {
+                            setSilver(e.target.value);
+                            if (e.target.value < 0) {
+                                alert("음수는 사용할 수 없습니다.");
+                                setSilver("");
+                                return;
+                            }
+                        }}
+                        type="number"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="">Bronze</label>
+                    <input
+                        value={bronze}
+                        onChange={(e) => {
+                            setBronze(e.target.value);
+                            if (e.target.value < 0) {
+                                alert("음수는 사용할 수 없습니다.");
+                                setBronze("");
+                                return;
+                            }
+                        }}
+                        type="number"
+                    />
+                </div>
+                <button
+                    onClick={() => {
+                        const newCountry = {
+                            id: uuid(),
+                            countryName: countryName,
+                            gold: gold,
+                            silver: silver,
+                            bronze: bronze,
+                        };
+                        setCountries([...countries, newCountry]);
+                        setTalk("");
+                    }}
+                >
+                    Add
+                </button>
                 <button>Update</button>
             </header>
             <main className="main-container">
-                <table className="table" cellSpacing="0">
-                    <thead>
-                        <tr>
-                            {tbodyTitle.map((title) => {
-                                return <th>{title}</th>;
+                {talk ? (
+                    <p>{talk}</p>
+                ) : (
+                    <table className="table" cellSpacing="0">
+                        <thead>
+                            <tr>
+                                {tbodyTitle.map((title) => {
+                                    return <th key={title}>{title}</th>;
+                                })}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {countries.map((country) => {
+                                return (
+                                    <CountryRow
+                                        key={country.id}
+                                        country={country}
+                                    />
+                                );
                             })}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {countries.map((country) => {
-                            return <County country={country} />;
-                        })}
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                )}
             </main>
         </div>
     );
@@ -64,12 +117,12 @@ const App = () => {
 
 export default App;
 
-const { country, gold, silver, bronze } = country;
+const CountryRow = ({ country }) => {
+    const { countryName, gold, silver, bronze } = country;
 
-const County = ({ country }) => {
     return (
         <tr>
-            <td>{country}</td>
+            <td>{countryName}</td>
             <td>{gold}</td>
             <td>{silver}</td>
             <td>{bronze}</td>
